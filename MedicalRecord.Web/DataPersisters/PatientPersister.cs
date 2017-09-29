5654eb0aa62cd935824ba030e439548277be4cbb
radoslav.sholev <radosholev@gmail.com>
2017-09-29 16:32:54 +0300

    remove test exception

--- MedicalRecord.Web/DataPersisters/PatientPersister.cs
+++ MedicalRecord.Web/DataPersisters/PatientPersister.cs
@@ -11,7 +11,6 @@ namespace MedicalRecord.Web.DataPersisters
         public PatientModel Get(int id)
         {
             PatientModel patientModel = null;
-            throw new Exception("Test exception");
             using (MedicalRecordContext context = new MedicalRecordContext())
             {
                 var patient = context.Patients.Include(x => x.TeethStatus).Include(x => x.Diseases).FirstOrDefault(x => x.Id == id);
